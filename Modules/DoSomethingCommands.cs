79c31d634cab4a90a12152691e3ec965e7ef729b
lamlp <nnghialam@gmail.com>
2023-01-27 17:05:53 +0700

    First commit

---
+++ Modules/DoSomethingCommands.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpi.Services
+{
+    // interation modules must be public and inherit from an IInterationModuleBase
+    public class DoSomethingCommands : InteractionModuleBase<SocketInteractionContext>
+    {
+        // dependencies can be accessed through Property injection, public properties with public setters will be set by the service provider
+        public InteractionService Commands { get; set; }
+        private DoSomethingService _doSomethingService { get; set; }
+
+        // constructor injection is also a valid way to access the dependecies
+        public DoSomethingCommands(DoSomethingService doSomethingService)
+        {
+            _doSomethingService = doSomethingService;
+        }
+
+        // /command!
+        [SlashCommand("hello", "hello")]
+        public async Task SayHello()
+        {
+            await RespondAsync($"Welcome to Dạ Hành!");
+        }
+
+        // /command!
+        [SlashCommand("open", "open your website on server machine!")]
+        public async Task OpenWebsite(string url)
+        {
+            await RespondAsync(await _doSomethingService.OpenWebsite(url));
+        }
+    }
+}
---
+++ Services/DoSomethingService.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace csharpi.Services
+{
+    public class DoSomethingService
+    {
+        public async Task<string> OpenWebsite(string url)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = "chrome";
+                process.StartInfo.Arguments = $"{url}";
+                process.Start();
+                await process.WaitForExitAsync();
+                return $"Opened {url} on server browser!";
+            }
+            catch(Exception ex)
+            {
+                return $"Can not open {url} because: {ex.Message}";
+            }
+        }
+    }
+}
